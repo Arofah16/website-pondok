92011f44ce360c60795775aef307f5e1f30c1a7e
arofah16 <arofahr06@gmail.com>
2020-12-02 09:52:14 +0700

    satu

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app>
+        <!-- <Nav/> -->
+        <router-view></router-view>
+    </v-app>
+</template>
+
+<script>
+    // import Nav from '@/views/nav.vue';
+
+    export default {
+        // components: { Nav }
+    }
+
+</script>
